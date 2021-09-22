e1c738060570976725a4b64121e25408b4e517a1
JeanVictorMachado <jeanvictor.arq@gmail.com>
2021-09-21 23:25:44 -0300

    feat: creating componets banner, ribbon and highlight with tests

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
 
@@ -5,5 +6,8 @@ module.exports = withPWA({
   pwa: {
     dest: 'public',
     disable: !isProd
+  },
+  images: {
+    domains: ['localhost', 'res.cloudinary.com']
   }
 })
