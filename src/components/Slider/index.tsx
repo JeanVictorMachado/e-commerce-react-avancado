cbc3dbb9eefaf3175798766211be71158b5631e1
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-07-08 14:31:25 -0300

    feat: creating gallery component and starting game page

--- src/components/Slider/index.tsx
+++ src/components/Slider/index.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react'
 import SlickSlider, { Settings } from 'react-slick'
 
 import * as S from './styles'
@@ -8,12 +9,15 @@ export type SliderProps = {
   settings: SliderSettings
 } & Settings
 
-const Slider = ({ children, settings }: SliderProps) => {
-  return (
-    <S.Wrapper>
-      <SlickSlider {...settings}>{children}</SlickSlider>
-    </S.Wrapper>
-  )
-}
+const Slider: React.ForwardRefRenderFunction<SlickSlider, SliderProps> = (
+  { children, settings },
+  ref
+) => (
+  <S.Wrapper>
+    <SlickSlider ref={ref} {...settings}>
+      {children}
+    </SlickSlider>
+  </S.Wrapper>
+)
 
-export default Slider
+export default forwardRef(Slider)
---
+++ src/pages/game/[slug].tsx
@@ -0,0 +1,5 @@
+import Game from 'screens/Game'
+
+export default function Index() {
+  return <Game />
+}
