2a6d798dc542681343e4e61e960faa2453032231
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-07-12 10:31:10 -0300

    feat: creating profile page

---
+++ src/pages/profile/me.tsx
@@ -0,0 +1,10 @@
+import FormProfile from 'components/FormProfile'
+import Profile from 'screens/Profile'
+
+export default function Me() {
+  return (
+    <Profile>
+      <FormProfile />
+    </Profile>
+  )
+}
