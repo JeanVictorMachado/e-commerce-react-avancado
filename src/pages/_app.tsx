16f4a146a9e7b75926c6961d646b43dd4d1d94ba
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-07-18 13:30:45 -0300

    feat: creating graphql config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,34 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
+import { ApolloProvider } from '@apollo/client'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import { ThemeProvider } from 'styled-components'
 
-import GlobalStyles from 'styles/global'
 import theme from 'styles/theme'
+import GlobalStyles from 'styles/global'
+import { useApollo } from 'utils/apollo'
 
 const App: React.FC<AppProps> = ({ Component, pageProps }) => {
   const AnyComponent = Component as any
+  const client = useApollo(pageProps.initialApoloState)
 
   return (
-    <ThemeProvider theme={theme}>
-      <Head>
-        <title>Won Games</title>
-        <link rel="shortcut icon" href="/img/favicon.png" />
-        <link rel="apple-touch-icon" href="/img/favicon.png" />
-        <link rel="manifest" href="/manifest.json" />
-        <meta
-          name="description"
-          content="A simple project starter to work with TypeScript, React, NextJS and Syled Components"
-        />
-      </Head>
-      <GlobalStyles />
-      <AnyComponent {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <Head>
+          <title>Won Games</title>
+          <link rel="shortcut icon" href="/img/favicon.png" />
+          <link rel="apple-touch-icon" href="/img/favicon.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta
+            name="description"
+            content="A simple project starter to work with TypeScript, React, NextJS and Syled Components"
+          />
+        </Head>
+        <GlobalStyles />
+        <AnyComponent {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   )
 }
 
