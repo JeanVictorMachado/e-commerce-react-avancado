672b142d077d0757626f076ff28e4fbbfe05e26b
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-07-20 13:42:57 -0300

    feat: starting dynamic home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,44 @@
 import Home, { HomeScreenProps } from 'screens/Home'
-import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
+
+import { initializeApollo } from 'utils/apollo'
+import { QueryHome } from 'graphql/generated/QueryHome'
+import { QUERY_HOME } from 'graphql/queries/home'
 
 export default function Index(props: HomeScreenProps) {
   return <Home {...props} />
 }
 
-export function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const {
+    data: { banners, newGames }
+  } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
-      newGames: gamesMock,
+      revalidate: 10,
+      banners: banners.map((banner) => ({
+        img: `http://localhost:1337${banner.image?.url}`,
+        title: banner.title,
+        subtitle: banner.subtitle,
+        buttonLabel: banner.button?.label,
+        buttonLink: banner.button?.link,
+        ...(banner.ribbon && {
+          ribbon: banner.ribbon.text,
+          ribbonColor: banner.ribbon.color,
+          ribbonSize: banner.ribbon.size
+        })
+      })),
+      newGames: newGames.map((game) => ({
+        title: game.name,
+        slug: game.slug,
+        developer: game.developers[0].name,
+        img: `http://localhost:1337${game.cover?.url}`,
+        price: game.price
+      })),
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
       upcomingGames: gamesMock,
