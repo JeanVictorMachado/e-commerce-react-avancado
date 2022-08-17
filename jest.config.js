cae2ba3b584311557639a4524c399ca1583239e4
JeanVictorMachado <jeanvictor.arq@gmail.com>
2022-08-16 22:01:17 -0300

    fix: correcting codes

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,11 @@ module.exports = {
     '!src/styles/**/*.ts',
     '!src/styles/**/*.ts',
     '!src/utils/apollo.ts',
+    '!src/graphql/queries/*.ts',
+    '!src/utils/apolloCache.ts',
     '!src/types/**/*.d.ts',
-    '!src/graphql/queries/*.ts'
+    '!src/graphql/**/*.ts',
+    '!src/**/mock.ts'
   ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
